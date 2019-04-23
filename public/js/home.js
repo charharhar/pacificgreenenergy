495b976bd62e71c44496d774c30e202855c4f28b
Charles Lee <charleslee90@gmail.com>
2019-04-24 00:11:34 +0800

    Animation hero, clouds, draft

--- public/js/home.js
+++ public/js/home.js
@@ -1,6 +1,11 @@
 
 import '../css/home.css';
 import { hotReload } from './util.js'
+import { TweenMax, TimelineLite } from "gsap/TweenMax";
+import ScrollMagic from 'scrollmagic';
+import 'animation.gsap'
+import 'debug.addIndicators'
+
 
 window.addEventListener('load', function(e) {
   console.log('--> Application started  |  Current route: /')
@@ -19,6 +24,46 @@ const figureMarginTop = (window.innerHeight - figureHeight) / 2
 
 stickyContainer.style.top = figureMarginTop
 
+/*
+ * ==============================================
+ *            Scroll Magic
+ * ==============================================
+ */
 
+const sceneOptionsGenerator = (offset = 0, duration = 0, triggerElement = null) => ({
+  offset,
+  duration,
+  triggerElement,
+});
+
+const sceneGenerator = (options, tween) => (
+  new ScrollMagic.Scene(options).setTween(tween)
+);
+
+const pgeController = new ScrollMagic.Controller();
+
+const heroTweenOne = TweenMax.fromTo('.hero-container', 1, { opacity: 1 }, { opacity: 0 });
+const heroOptionsOne = sceneOptionsGenerator((window.innerHeight/2), window.innerHeight, '.hero-dummy')
+
+const cloudTweenOne = TweenMax.fromTo('.cloud-container', 1, { yPercent: -15 }, { yPercent: 0 });
+const cloudOptionsOne = sceneOptionsGenerator((window.innerHeight/2), window.innerHeight, '.hero-dummy')
+
+const cloudLeftTween = TweenMax.fromTo('.cloud-left', 1, { xPercent: -5 }, { xPercent: -45, ease: Power2.easeIn });
+const cloudLeftOptions = sceneOptionsGenerator(window.innerHeight + (window.innerHeight/2), window.innerHeight, '.hero-dummy')
+
+const cloudRightTween = TweenMax.fromTo('.cloud-right', 1, { xPercent: 5 }, { xPercent: 45, ease: Power2.easeIn });
+const cloudRightOptions = sceneOptionsGenerator(window.innerHeight + (window.innerHeight/2), window.innerHeight, '.hero-dummy')
+
+pgeController.addScene([
+  sceneGenerator(heroOptionsOne, heroTweenOne),
+  sceneGenerator(cloudOptionsOne, cloudTweenOne),
+  sceneGenerator(cloudLeftOptions, cloudLeftTween),
+  sceneGenerator(cloudRightOptions, cloudRightTween),
+])
+/*
+ * ==============================================
+ *            Hot Reload
+ * ==============================================
+ */
 hotReload();
 
