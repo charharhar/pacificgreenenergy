a4cf4c0aa4d229ae0d3bd4464295d6fffef8fc0d
Charles Lee <charleslee90@gmail.com>
2019-04-23 17:13:22 +0800

    Adds basic structure

--- public/js/home.js
+++ public/js/home.js
@@ -6,4 +6,19 @@ window.addEventListener('load', function(e) {
   console.log('--> Application started  |  Current route: /')
 })
 
+/*
+ * ==============================================
+ *            Sticky Handlers
+ * ==============================================
+ */
+
+const stickyContainer = document.querySelector('.stickyContainer');
+
+const figureHeight = window.innerHeight / 2
+const figureMarginTop = (window.innerHeight - figureHeight) / 2
+
+stickyContainer.style.top = figureMarginTop
+
+
 hotReload();
+
