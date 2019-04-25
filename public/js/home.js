5b4f64516fb3a950a4d4955a0cd89808f577376e
Charles Lee <charleslee90@gmail.com>
2019-04-25 09:42:43 +0800

    Completes up to zoom in taiwan

--- public/js/home.js
+++ public/js/home.js
@@ -6,11 +6,6 @@ import ScrollMagic from 'scrollmagic';
 import 'animation.gsap'
 import 'debug.addIndicators'
 
-
-window.addEventListener('load', function(e) {
-  console.log('--> Application started  |  Current route: /')
-})
-
 /*
  * ==============================================
  *            Sticky Handlers
@@ -30,6 +25,9 @@ stickyContainer.style.top = figureMarginTop
  * ==============================================
  */
 
+let fullHeight = window.innerHeight
+let halfHeight = (fullHeight / 2)
+
 const sceneOptionsGenerator = (offset = 0, duration = 0, triggerElement = null) => ({
   offset,
   duration,
@@ -37,33 +35,79 @@ const sceneOptionsGenerator = (offset = 0, duration = 0, triggerElement = null) 
 });
 
 const sceneGenerator = (options, tween) => (
-  new ScrollMagic.Scene(options).setTween(tween)
+  new ScrollMagic.Scene(options).setTween(tween).addIndicators()
 );
+
+const fromToSceneGenerator = (target, duration, fromVar, toVar) => (
+  TweenMax.fromTo(target, duration, fromVar, Object.assign({
+    overwrite: false,
+  }, toVar))
+)
+
+const basicTweenGenerator = (target, duration = 0, vars, delay = 0) => (
+  TweenMax.to(target, duration, {
+    css: vars,
+    delay: delay,
+  })
+)
+
+function resizeHandler() {
+  fullHeight = window.innerHeight;
+  halfHeight = (fullHeight / 2);
+}
 
 const pgeController = new ScrollMagic.Controller();
 
-const heroTweenOne = TweenMax.fromTo('.hero-container', 1, { opacity: 1 }, { opacity: 0 });
-const heroOptionsOne = sceneOptionsGenerator((window.innerHeight/2), window.innerHeight, '.hero-dummy')
+// TWEENS
 
-const cloudTweenOne = TweenMax.fromTo('.cloud-container', 1, { yPercent: -15 }, { yPercent: 0 });
-const cloudOptionsOne = sceneOptionsGenerator((window.innerHeight/2), window.innerHeight, '.hero-dummy')
+const heroTweenOne = fromToSceneGenerator('.hero-container', 1, { opacity: 1 }, { opacity: 0 });
+const heroOptionsOne = sceneOptionsGenerator(halfHeight, fullHeight, '.hero-dummy')
 
-const cloudLeftTween = TweenMax.fromTo('.cloud-left', 1, { xPercent: -5 }, { xPercent: -45, ease: Power2.easeIn });
-const cloudLeftOptions = sceneOptionsGenerator(window.innerHeight + (window.innerHeight/2), window.innerHeight, '.hero-dummy')
+const cloudTweenOne = fromToSceneGenerator('.cloud-container', 1, { yPercent: -30 }, { yPercent: 0 });
+const cloudOptionsOne = sceneOptionsGenerator(halfHeight, fullHeight, '.hero-dummy')
 
-const cloudRightTween = TweenMax.fromTo('.cloud-right', 1, { xPercent: 5 }, { xPercent: 45, ease: Power2.easeIn });
-const cloudRightOptions = sceneOptionsGenerator(window.innerHeight + (window.innerHeight/2), window.innerHeight, '.hero-dummy')
+const cloudLeftTween = fromToSceneGenerator('.cloud-left', 1, { xPercent: -5 }, { xPercent: -45, ease: Power2.easeInOut });
+const cloudLeftOptions = sceneOptionsGenerator(fullHeight + halfHeight, 0, '.hero-dummy')
+
+const cloudRightTween = fromToSceneGenerator('.cloud-right', 1, { xPercent: 5 }, { xPercent: 45, ease: Power2.easeInOut });
+const cloudRightOptions = sceneOptionsGenerator(fullHeight + halfHeight, 0, '.hero-dummy')
+
+const logoTweenOne = basicTweenGenerator('.logo-container', 1, { opacity: 1 });
+const logoOptionsOne = sceneOptionsGenerator(fullHeight + halfHeight, 0, '.hero-dummy')
+
+const logoTweenTwo = basicTweenGenerator('.logo-container', 1, { opacity: 0 });
+const logoOptionsTwo = sceneOptionsGenerator(0, 0, '.taiwan-intro-scene')
+
+const taiwanTweenOne = basicTweenGenerator('.taiwan-container', 1, { opacity: 1 });
+const taiwanOptionsOne = sceneOptionsGenerator(0, 0, '.taiwan-intro-scene')
+
+const taiwanZoomTween = basicTweenGenerator('.pge-taiwan-img', 1, { scale: 1 });
+const taiwanZoomOptions = sceneOptionsGenerator(0, 0, '.taiwan-zoom-scene')
+
+const cloudZoomTween = basicTweenGenerator('.cloud-container', .7, { scale: 4, opacity: 0, yPercent: -30 });
+const cloudZoomOptions = sceneOptionsGenerator(0, 0, '.taiwan-zoom-scene')
 
 pgeController.addScene([
   sceneGenerator(heroOptionsOne, heroTweenOne),
   sceneGenerator(cloudOptionsOne, cloudTweenOne),
   sceneGenerator(cloudLeftOptions, cloudLeftTween),
   sceneGenerator(cloudRightOptions, cloudRightTween),
+  sceneGenerator(logoOptionsOne, logoTweenOne),
+  sceneGenerator(logoOptionsTwo, logoTweenTwo),
+  sceneGenerator(taiwanOptionsOne, taiwanTweenOne),
+  sceneGenerator(taiwanZoomOptions, taiwanZoomTween),
+  sceneGenerator(cloudZoomOptions, cloudZoomTween),
 ])
+
 /*
  * ==============================================
- *            Hot Reload
+ *            Event Listeners
  * ==============================================
  */
+
+window.addEventListener('load', function(e) {
+  resizeHandler()
+})
+
 hotReload();
 
