b17376394ac011d705a89221bc3da1af04d55776
Charles Lee <charleslee90@gmail.com>
2019-08-22 12:24:19 +0800

    Mobile nav complete

--- public/js/faq.js
+++ public/js/faq.js
@@ -1,6 +1,18 @@
 
 import '../css/faq.css';
-import { sliceArray, hotReload } from './util.js'
+import {
+  sliceArray,
+  hotReload,
+  mobileNavHandler,
+} from './util.js'
+
+/**
+ * Moible Nav Handler
+ */
+const hamburger = document.querySelector('.hamburger');
+const mobileNavList = document.querySelector('.mobile-nav-list');
+
+mobileNavHandler(hamburger, mobileNavList);
 
 /**
  * Event Handlers
