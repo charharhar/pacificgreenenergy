a11df7bcd54c12cc4811fc14f7745ceeda805d9a
Charles Lee <charleslee90@gmail.com>
2019-09-17 10:53:39 +0800

    Lots of bug fixes

--- public/js/contact.js
+++ public/js/contact.js
@@ -70,7 +70,7 @@ window.addEventListener('load', function(e) {
     const formNames = checkedRadio === 'land' ? {...formBase, ...formLand} : {...formBase, ...formStructure}
     const formData = $('#pge-form').serializeArray()
 
-    const mailTarget = 'ccheng.pge@gmail.com';
+    const mailTarget = 'sales@pacgenergy.com';
     const mailSubject = `我要支持綠能 - ${formData[0].value}`;
 
     const mailBody = formData.reduce((acc, cur) => {
