545b02e077f513b48c4860bafcba555b7d05e948
Charles Lee <charleslee90@gmail.com>
2019-10-16 17:07:06 +0800

    Completes email frontend section, ignores lambda

--- public/js/contact.js
+++ public/js/contact.js
@@ -42,43 +42,75 @@ sliceArray(document.querySelectorAll('.pge-radio')).forEach(radio => {
  * Form button submit handler
  */
 window.addEventListener('load', function(e) {
+  const formBase = {
+    name: '姓名',
+    phone: '聯絡電話',
+    email: '電子信箱',
+    city: '縣市',
+    estate: '我有',
+  }
+
+  const formLand = {
+    town: '鎮市區',
+    lot: '地段',
+    number: '地號',
+  }
+
+  const formStructure = {
+    address: '門牌地址',
+  }
+
+  function post(url, body, callback) {
+    const req = new XMLHttpRequest();
+    req.open('POST', url, true);
+    req.setRequestHeader('Content-Type', 'application/json');
+    req.addEventListener('load', function() {
+      if (req.status < 400) {
+        callback(null, JSON.parse(req.responseText));
+      } else {
+        callback(new Error("Request failed: " + req.statusText));
+      }
+    })
+
+    req.send(JSON.stringify(body));
+  }
+
+  function success() {
+    Swal.fire({
+      type: 'success',
+      title: '發信成功',
+      text: '謝謝您支持綠能！我們會跟你',
+    })
+  }
+
+  function error(err) {
+    Swal.fire({
+      type: 'error',
+      title: '發生問題',
+      text: '請直接發信到 sales@pacgenergy.com',
+    })
+  }
+
   $('#form-button').click(function(e) {
     e.preventDefault();
 
-    const formBase = {
-      name: '姓名',
-      phone: '聯絡電話',
-      email: '電子信箱',
-      city: '縣市',
-      estate: '我有',
-    }
-
-    const formLand = {
-      town: '鎮市區',
-      lot: '地段',
-      number: '地號',
-    }
-
-    const formStructure = {
-      address: '門牌地址',
-    }
-
-    let hasError = true;
+    const url = 'https://m5wrd3lwg1.execute-api.us-east-1.amazonaws.com/dev/';
     const captchaResponse = grecaptcha.getResponse();
     const checkedRadio = document.querySelector('.pge-radio:checked').getAttribute('id');
+    const errorCatch = {
+      formVals: false,
+      captcha: false,
+    }
 
     const formNames = checkedRadio === 'land' ? {...formBase, ...formLand} : {...formBase, ...formStructure}
     const formData = $('#pge-form').serializeArray()
 
-    const mailTarget = 'sales@pacgenergy.com';
-    const mailSubject = `我要支持綠能 - ${formData[0].value}`;
-
     const mailBody = formData.reduce((acc, cur) => {
       if (checkedRadio === 'land' && cur.name === 'number') {
-        hasError = !cur.value
+        errorCatch.formVals = !cur.value
       }
       if (checkedRadio === 'structure' && cur.name === 'address') {
-        hasError = !cur.value
+        errorCatch.formVals = !cur.value
       }
 
       return formNames.hasOwnProperty(cur.name)
@@ -86,11 +118,25 @@ window.addEventListener('load', function(e) {
         : acc;
     }, '')
 
-    hasError = !!captchaResponse;
+    errorCatch.captcha = !captchaResponse;
 
-    if (!hasError) {
-      $('#pge-form-submit').attr('href', `mailto:${mailTarget}?subject=${mailSubject}&body=${mailBody}`);
-      $('#pge-form-submit')[0].click()
+    const payload = formData.reduce((acc, cur) => {
+      if (formNames.hasOwnProperty(cur.name)) {
+        acc[cur.name] = cur.value
+      }
+
+      return acc;
+    }, { captchaResponse, mailBody })
+
+    if (!errorCatch.formVals && !errorCatch.captcha) {
+      Swal.fire({
+        title: '發信中...',
+        onOpen: () => { Swal.showLoading() },
+      })
+      post(url, payload, (err, res) => {
+        if (err) { return error(err) };
+        success();
+      })
     } else {
       Swal.fire({
         type: 'error',
